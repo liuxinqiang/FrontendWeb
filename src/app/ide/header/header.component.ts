b28b4a159e1ed541b467793dad0fede81eee7952
liuxinqiang <me@liuxinqiang.com>
2019-01-10 14:16:01 +0800

    添加退出导航功能

--- src/app/ide/header/header.component.ts
+++ src/app/ide/header/header.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DomService} from '../../common/services/dom.service';
 
 @Component({
     selector: 'app-ide-header',
@@ -6,13 +7,33 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
+    @Input() backUrl = '/';
+    fullScreenMode = false;
 
     headerDropConfig = 'mode: click;offset:11;animation: ui-animation-slide-top-small';
 
-    constructor() {
+    constructor(
+        private _domService: DomService,
+    ) {
     }
 
     ngOnInit() {
+    }
+
+    fullScreen() {
+        this._domService.enableFullScreen()
+            .then(() => {
+                this.fullScreenMode = true;
+                // this.editorManagerService.layout();
+            });
+    }
+
+    exitFullScreen() {
+        this._domService.disableFullScreen()
+            .then(() => {
+                this.fullScreenMode = false;
+                // this.editorManagerService.layout();
+            });
     }
 
 }
